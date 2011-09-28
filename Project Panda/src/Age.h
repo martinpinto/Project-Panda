7b1beb8406b7d28b252c6ea8fa6f95c7782253e9
Martin Prodanov <martin.prodanov@gmail.com>
2011-09-28 15:52:17 +0200

    Age mechanics implemented based on current time and a birthdata. Default is: a gotshi ages for a day 1 year

--- Project Panda/src/Age.h
+++ Project Panda/src/Age.h
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <vector>
-#include <time.h>
+#include <ctime>
 
 #ifndef _AGE_H_
 #define _AGE_H_
 
 class Age {
-      typedef time_t Date;
-      
-      public:
-             Age();
-             int getCurrentAge();
-             void setCurrentAge(int age);
-             Date getBirthday();
-      
-      private:
-              int age;
-              Date birthday;
+
+public:
+	Age();
+	Age(int agingFactor);
+	int getCurrentAge();
+	long getBirthday();
+
+private:
+	int age;
+	int agingFactor; // causes the gotshi to age a factor faster
+	long birthday;
+	int computeCurrentAge();
 };
 #endif
--- Project Panda/src/main.cpp
+++ Project Panda/src/main.cpp
@@ -1,7 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include "Character.h"
-#include "AgeMechanics.h"
+#include "Utils.h"
 #include "EatMechanics.h"
 #include "CleanMechanics.h"
 #include "LearningMechanics.h"
@@ -27,76 +27,85 @@ void testPlayMechanics(Character character);
 void testSleepMechanics(Character character);
 void testTemperatureMechanics(Character character);
 
-int main(int argc, char *argv[])
-{
-    Character panda;
-    
-    // initializing Character "panda"
-    initCharacter(panda);
-    
-    // testing the aging system
-    testAgeMechanics(panda);
-    
-    system("PAUSE");
-    return EXIT_SUCCESS;
+int main(int argc, char *argv[]) {
+	Character panda;
+
+	// initializing Character "panda"
+	initCharacter(panda);
+
+	// testing the aging system
+	testAgeMechanics(panda);
+
+	system("PAUSE");
+	return EXIT_SUCCESS;
 }
 
 void initCharacter(Character character) {
-     
+
 }
 
 void testAgeMechanics(Character character) {
-     int n;
-     cout << "Starting countdown...\n" << endl;
-     AgeMechanics ageMechanics;
-     for (n=10; n>0; n--) {
-         cout << n << endl;
-         ageMechanics.wait(1);
-     }
-     cout << "Finished!!!\n" << endl;
-     Age age;
-     age.setCurrentAge(3);
-     cout << "Age before: " + age.getCurrentAge() << endl;
-     age = ageMechanics.increaseAge(age);
-     cout << "Age increased: " + age.getCurrentAge() << endl;
+
+	int n = 10; // seconds to wait and years the gotshi to age
+	Utils utils;
+	//new Age growing N times faster 86400 Each second is 1 gotshi year
+	Age age = Age(86400);
+	time_t birthday = age.getBirthday();
+	cout << "Date of Birth: " << ctime(&birthday) << endl;
+	cout << "Age before: " << age.getCurrentAge() << endl;
+
+	cout << "Starting countdown...\n" << endl;
+	for (int i = n; i > 0; i--) {
+		cout << i << endl;
+		utils.wait(1);
+	}
+	//after 10s gotshi must be 10 years old
+	if (age.getCurrentAge() == n) {
+		cout << "Age after : " << age.getCurrentAge() << endl;
+		cout << "Test OK. Gotshi ages properly" << endl;
+	} else {
+		cout << "Failed: gotshi must be " << n << " years old ";
+		cout << "but was: " << age.getCurrentAge() << endl;
+	}
+	cout << "Finished testing Aging mechanics!!!" << endl;
 }
 
 void testCleanMechanics(Character character) {
-     
+
 }
 
 void testEatingMechanics(Character character) {
-     
+
 }
 
 void testLearningMechanics(Character character) {
-     
+
 }
 
 void testMedicineMechanics(Character character) {
-     
+
 }
 
 void testNeedMechanics(Character character) {
-     
+
 }
 
 void testWantMechanics(Character character) {
-     
+
 }
 
 void testMoodMechanics(Character character) {
-     
+
 }
 
 void testPlayMechanics(Character character) {
-     
+
 }
 
 void testSleepMechanics(Character character) {
-     
+
 }
 
 void testTemperatureMechanics(Character character) {
-     
+
 }
